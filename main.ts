22d15a876a2a65f580485ccbf3bd5728aa1f33ad
redbeanicy <117802428+redbeanicy@users.noreply.github.com>
2022-11-09 16:41:51 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,27 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    frdDice = receivedNumber
+})
+input.onGesture(Gesture.Shake, function () {
+    myDice = randint(1, 6)
+    radio.sendNumber(myDice)
+    basic.showNumber(myDice)
+    basic.pause(1000)
+})
+let frdDice = 0
+let myDice = 0
+myDice = 0
+frdDice = 0
+basic.forever(function () {
+    if (myDice != 0 && frdDice != 0) {
+        basic.pause(1000)
+        if (myDice == frdDice) {
+            basic.showIcon(IconNames.Asleep)
+        } else if (myDice > frdDice) {
+            basic.showIcon(IconNames.Happy)
+        } else {
+            basic.showIcon(IconNames.Sad)
+        }
+        myDice = 0
+        frdDice = 0
+    }
+})
